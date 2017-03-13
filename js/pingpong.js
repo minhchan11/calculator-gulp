246afc2814bc42e02685a262526d2a834477662d
Minh Phuong and Renee Mei <minh+renee@>
2017-03-13 09:56:14 -0700

    add all functionality

--- js/pingpong.js
+++ js/pingpong.js
@@ -1,4 +1,8 @@
-function pingPong(goal) {
+function Calculator(skinName) {
+  this.skin = skinName;
+}
+
+Calculator.prototype.pingPong= function(goal) {
   var output = [];
   for (var i = 1; i <= goal; i++) {
     if (i % 15 === 0) {
@@ -12,15 +16,6 @@ function pingPong(goal) {
     }
   }
   return output;
-}
+};
 
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
+exports.calculatorModule = Calculator;
